98f13ce1595092d1f3a8d462bca81754c231a6f6
Franco <frfantusatti@protonmail.com>
2024-11-26 05:12:44 -0300

    add webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // Detecta archivos CSS
+        use: ['style-loader', 'css-loader'], // Procesa e inyecta CSS
+      },
+    ],
+  },
 };
